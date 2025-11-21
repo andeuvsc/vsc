3ce8456b0c71ddb4da47336ef88b1190d5c077de
andwk23 <fuerschrott@online.de>
2025-11-21 18:07:43 +0100

    Vanilla 2

---
+++ JavaScript/JSCampus/Visitenkarte/card-vanilla.js
@@ -0,0 +1,17 @@
+console.clear;
+
+const firstName = "Max";
+const lastName = "Mustermann";
+const job = "Webentwickler";
+const tele = "+49 123456789";
+const mail = "max_mustermann@example.com";
+
+const person = `
+${firstName} ${lastName}
+${job}
+Tel.: ${tele}
+Mail: ${mail}
+`
+
+
+console.log(person);
